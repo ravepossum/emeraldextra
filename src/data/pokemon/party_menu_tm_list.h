2d5e11f668f2a2ba18aab3dcbd05dca853f3dd0e
Payton O'Daniels <payton.odaniels@gmail.com>
2023-09-14 21:24:25 -0400

    Updating learnsets to gen 8 ones for most mons, adding new pokemon to route 101, adding colors to nature altered stats.

---
+++ src/data/pokemon/party_menu_tm_list.h
@@ -0,0 +1,6 @@
+// IMPORTANT: DO NOT PASTE THIS FILE INTO YOUR REPO!
+// Instead, paste the following array into src/data/party_menu.h
+
+const u16 sTMHMMoves[TMHM_COUNT] =
+{
+};
---
+++ src/data/pokemon/party_menu_tutor_list.h
@@ -0,0 +1,4 @@
+// IMPORTANT: DO NOT PASTE THIS FILE INTO YOUR REPO!
+// Instead, paste the following list of defines into include/constants/party_menu.h
+
+#define TUTOR_MOVE_COUNT               0
