58f27e2be6b7f919089c6f68a7b9ee38482972ce
ravepossum <ravepossum@example.com>
2023-10-13 21:01:14 -0400

    Removed extra save dialogue

--- src/start_menu.c
+++ src/start_menu.c
@@ -1088,7 +1088,7 @@ static u8 SaveFileExistsCallback(void)
     }
     else
     {
-        ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
+        sSaveDialogCallback = SaveSavingMessageCallback;
     }
 
     return SAVE_IN_PROGRESS;
