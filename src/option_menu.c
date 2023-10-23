c9b75d2d77cb0dc1830490ddae24e559d35cc7b4
ravepossum <ravepossum@example.com>
2023-10-23 15:26:33 -0400

    Letting you skip intro screen, changing copyright year, changing main menu and options menu BG color

--- src/option_menu.c
+++ src/option_menu.c
@@ -132,7 +132,7 @@ static const struct BgTemplate sOptionMenuBgTemplates[] =
     }
 };
 
-static const u16 sOptionMenuBg_Pal[] = {RGB(17, 18, 31)};
+static const u16 sOptionMenuBg_Pal[] = {RGB(1, 19, 13)};
 
 static void MainCB2(void)
 {
