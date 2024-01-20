a817bc2b60427ef10b925d4a8f6683ec6d1ac8d1
ravepossum <ravepossum@example.com>
2024-01-20 16:22:39 -0500

    don't repeat pop-ups on same map sec id

--- src/overworld.c
+++ src/overworld.c
@@ -851,8 +851,7 @@ if (I_VS_SEEKER_CHARGING != 0)
     ResetFieldTasksArgs();
     RunOnResumeMapScript();
 
-    if (gMapHeader.regionMapSectionId != MAPSEC_BATTLE_FRONTIER
-     || gMapHeader.regionMapSectionId != sLastMapSectionId)
+    if (gMapHeader.regionMapSectionId != sLastMapSectionId)
         ShowMapNamePopup();
 }
 
