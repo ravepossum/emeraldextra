01796921c576d36a70c4843af9c16788d71ec73e
ravepossum <ravepossum@example.com>
2024-01-22 15:58:31 -0500

    fix tm case window border gfx when reopening after selecting tm

--- src/tm_case.c
+++ src/tm_case.c
@@ -1019,7 +1019,7 @@ static void InitWindowTemplatesAndPals(void)
     InitWindows(sWindowTemplates);
     DeactivateAllTextPrinters();
     LoadMessageBoxGfx(0, 0x64, BG_PLTT_ID(11));
-    LoadUserWindowBorderGfx(0, 0x78, BG_PLTT_ID(13));
+    LoadUserWindowBorderGfx(0, 0x78, BG_PLTT_ID(14));
     LoadPalette(gTMCaseMainWindowPalette, BG_PLTT_ID(15), PLTT_SIZE_4BPP);
     LoadPalette(gTMCaseMainWindowPalette, BG_PLTT_ID(10), PLTT_SIZE_4BPP);
     LoadPalette(sPal3Override, BG_PLTT_ID(15) + 6, sizeof(sPal3Override));
